71b272b882e69e0b2a5128cf7e8d1d9a38694b91
mzr312312 <mzr312@gmailcom>
2025-01-06 13:37:56 +0800

    定期同步

--- Time_Series_Data_Processing/get_data_from_api.py
+++ Time_Series_Data_Processing/get_data_from_api.py
@@ -52,9 +52,9 @@ def convert_tag_value(value):
 url = 'http://10.86.6.3:8081/japrojecttag/timeseries'
 
 # 设置时间范围
-start_time = "2024-11-22 00:00:00"
-end_time = "2024-11-23 00:03:00"
-granularity_minutes = 10
+start_time = "2025-01-05 00:00:00"
+end_time = "2025-01-06 00:03:00"
+granularity_minutes = 5
 tagCodes = generate_tagcodes()  # 从tagcode_generator.py 导入的函数，生成tagCodes列表
 # 用于存储所有 DataFrame 的列表
 all_data_frames = []
